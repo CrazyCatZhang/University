0f9c153095d3c195d7b642b34e3bc7e9676a1d7f
Peach Zhang <CrazyCatZhang@outlook.com>
2021-06-08 00:16:24 +0800

    Update datas.js

--- IM/commons/js/datas.js
+++ IM/commons/js/datas.js
@@ -132,5 +132,179 @@ export default {
 			},
 		];
 		return isfriend;
+	},
+
+	//聊天消息
+	message: function() {
+		let msgs = [{
+				id: 'b', //用户id
+				imgurl: 'two.png',
+				message: {
+					name: '天安门城楼售票处',
+					address: '东长安街天安门城楼内',
+					latitude: '39.909473',
+					longitude: '116.39730899999999'
+				},
+				types: 3, //内容类型（0文字，1图片链接，2音频连接...)
+				time: new Date(), //发送时间
+				tip: 18,
+			},
+			{
+				id: 'a', //用户id
+				imgurl: 'one.png',
+				message: {
+					name: '天安门城楼售票处天安门城楼售票处',
+					address: '东长安街天安门城楼内东长安街天安门城楼内',
+					latitude: '39.909473',
+					longitude: '116.39730899999999'
+				},
+				types: 3, //内容类型（0文字，1图片链接，2音频连接...)
+				time: new Date(), //发送时间
+				tip: 17,
+			},
+			{
+				id: 'a', //用户id
+				imgurl: 'one.png',
+				message: {
+					voice: 'a',
+					time: 60
+				},
+				types: 2, //内容类型（0文字，1图片链接，2音频连接...)
+				time: new Date(), //发送时间
+				tip: 15,
+			},
+			{
+				id: 'b', //用户id
+				imgurl: 'two.png',
+				message: {
+					voice: 'a',
+					time: 20
+				},
+				types: 2, //内容类型（0文字，1图片链接，2音频连接...)
+				time: new Date(), //发送时间
+				tip: 16,
+			},
+			{
+				id: 'a', //用户id
+				imgurl: 'one.png',
+				message: '世界那么大 有人给你难堪 有人为你解围 爱你的人永远知道怎么爱你',
+				types: 0, //内容类型（0文字，1图片链接，2音频连接...)
+				time: new Date() - 1000, //发送时间
+				tip: 0,
+			},
+			{
+				id: 'a', //用户id
+				imgurl: 'one.png',
+				message: '世界那么大 有人给你难堪 有人为你解围 爱你的人永远知道怎么爱你',
+				types: 0, //内容类型（0文字，1图片链接，2音频连接...)
+				time: new Date() - 1000 * 16, //发送时间
+				tip: 1,
+			},
+			{
+				id: 'b', //用户id
+				imgurl: 'two.png',
+				message: '世界那么大 有人给你难堪 有人为你解围 爱你的人永远知道怎么爱你',
+				types: 0, //内容类型（0文字，1图片链接，2音频连接...)
+				time: new Date() - 1000 * 60, //发送时间
+				tip: 2,
+			},
+			{
+				id: 'a', //用户id
+				imgurl: 'one.png',
+				message: '世界那么大 有人给你难堪 有人为你解围 爱你的人永远知道怎么爱你',
+				types: 0, //内容类型（0文字，1图片链接，2音频连接...)
+				time: new Date() - 1000 * 60 * 36, //发送时间
+				tip: 3,
+			},
+			{
+				id: 'a', //用户id
+				imgurl: 'one.png',
+				message: 'one.png',
+				types: 1, //内容类型（0文字，1图片链接，2音频连接...)
+				time: new Date() - 1000 * 60 * 40, //发送时间
+				tip: 4,
+			},
+			{
+				id: 'b', //用户id
+				imgurl: 'one.png',
+				message: 'si.jpg',
+				types: 1, //内容类型（0文字，1图片链接，2音频连接...)
+				time: new Date() - 1000 * 60 * 57, //发送时间
+				tip: 5,
+			},
+			{
+				id: 'a', //用户id
+				imgurl: 'one.png',
+				message: 'wu.jpg',
+				types: 1, //内容类型（0文字，1图片链接，2音频连接...)
+				time: new Date() - 1000 * 60 * 58, //发送时间
+				tip: 6,
+			},
+			{
+				id: 'b', //用户id
+				imgurl: 'one.png',
+				message: '世界那么大 有人给你难堪 有人为你解围 爱你的人永远知道怎么爱你',
+				types: 0, //内容类型（0文字，1图片链接，2音频连接...)
+				time: new Date() - 1000 * 60 * 60, //发送时间
+				tip: 7,
+			},
+			{
+				id: 'b', //用户id
+				imgurl: 'one.png',
+				message: '世界那么大 有人给你难堪 有人为你解围 爱你的人永远知道怎么爱你',
+				types: 0, //内容类型（0文字，1图片链接，2音频连接...)
+				time: new Date() - 1000 * 60 * 58 * 24, //发送时间
+				tip: 8,
+			},
+			{
+				id: 'a', //用户id
+				imgurl: 'one.png',
+				message: '世界那么大 有人给你难堪 有人为你解围 爱你的人永远知道怎么爱你',
+				types: 0, //内容类型（0文字，1图片链接，2音频连接...)
+				time: new Date() - 1000 * 60 * 60 * 24, //发送时间
+				tip: 9,
+			},
+			{
+				id: 'a', //用户id
+				imgurl: 'one.png',
+				message: 'er.jpg',
+				types: 1, //内容类型（0文字，1图片链接，2音频连接...)
+				time: new Date() - 1000 * 60 * 60 * 24, //发送时间
+				tip: 10,
+			},
+			{
+				id: 'b', //用户id
+				imgurl: 'one.png',
+				message: '世界那么大 有人给你难堪 有人为你解围 爱你的人永远知道怎么爱你',
+				types: 0, //内容类型（0文字，1图片链接，2音频连接...)
+				time: new Date() - 1000 * 60 * 60 * 24, //发送时间
+				tip: 11,
+			},
+			{
+				id: 'a', //用户id
+				imgurl: 'one.png',
+				message: 'yi.jpg',
+				types: 1, //内容类型（0文字，1图片链接，2音频连接...)
+				time: new Date() - 1000 * 60 * 60 * 240, //发送时间
+				tip: 12,
+			},
+			{
+				id: 'b', //用户id
+				imgurl: 'one.png',
+				message: '世界那么大 有人给你难堪 有人为你解围 爱你的人永远知道怎么爱你',
+				types: 0, //内容类型（0文字，1图片链接，2音频连接...)
+				time: new Date() - 1000 * 60 * 60 * 241, //发送时间
+				tip: 13,
+			},
+			{
+				id: 'b', //用户id
+				imgurl: 'one.png',
+				message: '世界那么大 有人给你难堪 有人为你解围 爱你的人永远知道怎么爱你',
+				types: 0, //内容类型（0文字，1图片链接，2音频连接...)
+				time: new Date() - 1000 * 60 * 60 * 241, //发送时间
+				tip: 14,
+			},
+		];
+		return msgs;
 	}
 }
