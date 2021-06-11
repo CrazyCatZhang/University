117fa4b3b2dc6bfe7a58d287ca3715a612b7f706
Peach Zhang <CrazyCatZhang@outlook.com>
2021-06-11 20:36:11 +0800

    Create jwt.js

---
+++ Server/dao/jwt.js
@@ -0,0 +1,30 @@
+//token
+//引入token
+var jwt = require('jsonwebtoken');
+var secret = 'yikeshiguang';
+
+//生成token
+exports.generateToken = function (e) {
+    let payload = {
+        id: e,
+        time: new Date(),
+    };
+
+    let token = jwt.sign(payload, secret, { expiresIn: 60 * 60 * 24 * 120 });
+
+    return token;
+}
+
+//解码token
+exports.verifyToken = function (e) {
+    let payload;
+    jwt.verify(e, secret, function (err, result) {
+        if (err) {
+            payload = 0;
+        } else {
+            payload = 1;
+        }
+    });
+
+    return payload;
+}
