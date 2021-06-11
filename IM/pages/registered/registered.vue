1550be4e74d9bb17ce664af90ab5532c4c444a14
Peach Zhang <CrazyCatZhang@outlook.com>
2021-06-11 20:38:32 +0800

    Update registered.vue

--- IM/pages/registered/registered.vue
+++ IM/pages/registered/registered.vue
@@ -8,12 +8,12 @@
 			<view class="title">注册</view>
 			<view class="inputs">
 				<view class="inputs-div">
-					<input type="text" placeholder="请输入用户名" class="user" placeholder-style="color:#aaa;font-weight:400;" @input="getUser" />
+					<input type="text" placeholder="请输入用户名" class="user" placeholder-style="color:#aaa;font-weight:400;" @blur="matchUser" />
 					<view class="employ" v-if="useremploy">该用户名已被使用</view>
 					<image src="../../static/images/sign/right1.png" mode="" class="ok" v-if="isuser"></image>
 				</view>
 				<view class="inputs-div">
-					<input type="text" placeholder="请输入邮箱" class="email" placeholder-style="color:#aaa;font-weight:400;" @input="isEmail" />
+					<input type="text" placeholder="请输入邮箱" class="email" placeholder-style="color:#aaa;font-weight:400;" @blur="isEmail" />
 					<view class="employ" v-if="emailemploy">该邮箱已被使用</view>
 					<view class="invalid" v-if="invalid">无效邮箱</view>
 					<image src="../../static/images/sign/right1.png" mode="" class="ok" v-if="isemail"></image>
@@ -24,7 +24,7 @@
 				</view>
 			</view>
 		</view>
-		<view :class="[{ submit: isok }, { submit1: !isok }]">注册</view>
+		<view :class="[{ submit: isok }, { submit1: !isok }]" @tap="signUp">注册</view>
 	</view>
 </template>
 
@@ -45,6 +45,18 @@ export default {
 			pwd: '',
 			isok: false
 		};
+	},
+	computed: {
+		//判断是否可以注册
+		isOk: function() {
+			let that = this;
+			if (that.isuser && that.isemail && that.pwd.length > 7) {
+				that.isok = true;
+			} else {
+				that.isok = false;
+			}
+			return that.isok;
+		}
 	},
 	methods: {
 		//密码显示隐藏
@@ -67,36 +79,144 @@ export default {
 			if (this.email.length > 0) {
 				if (reg.test(this.email)) {
 					this.invalid = false;
+					//后端验证邮箱是否已被占用
+					this.matchMail();
 				} else {
 					this.invalid = true;
+					this.emailemploy = false;
+					this.isemail = false;
 				}
+			} else {
+				this.invalid = false;
+				this.emailemploy = false;
+				this.isemail = false;
+				this.isOk;
 			}
-			this.isOk();
 		},
 
-		getUser: function(e) {
+		//匹配邮箱
+		matchMail: function(e) {
+			uni.request({
+				url: this.serverUrl + '/signup/judge',
+				data: {
+					data: this.email,
+					type: 'email'
+				},
+				method: 'POST',
+				success: data => {
+					// console.log(data);
+					let status = data.data.status;
+					if (status == 200) {
+						let res = data.data.result;
+						if (res > 0) {
+							//表示邮箱已存在
+							this.emailemploy = true;
+							this.isemail = false;
+						} else {
+							//表示邮箱不存在
+							this.emailemploy = false;
+							this.isemail = true;
+						}
+						this.isOk;
+					} else if (status == 500) {
+						uni.showToast({
+							title: '服务器出错啦！',
+							icon: 'none',
+							duration: 2000
+						});
+					}
+				}
+			});
+		},
+
+		//匹配用户名
+		matchUser: function(e) {
 			this.user = e.detail.value;
-			this.isOk();
+			if (this.user.length > 0) {
+				uni.request({
+					url: this.serverUrl + '/signup/judge',
+					data: {
+						data: this.user,
+						type: 'name'
+					},
+					method: 'POST',
+					success: data => {
+						// console.log(data);
+						let status = data.data.status;
+						if (status == 200) {
+							let res = data.data.result;
+							if (res > 0) {
+								//表示用户已存在
+								this.useremploy = true;
+								this.isuser = false;
+							} else {
+								//表示用户不存在
+								this.useremploy = false;
+								this.isuser = true;
+							}
+							this.isOk;
+						} else if (status == 500) {
+							uni.showToast({
+								title: '服务器出错啦！',
+								icon: 'none',
+								duration: 2000
+							});
+						}
+					}
+				});
+			} else {
+				this.useremploy = false;
+				this.isuser = false;
+				this.isOk;
+			}
 		},
 
 		getPwd: function(e) {
 			this.pwd = e.detail.value;
-			this.isOk();
-		},
-
-		//判断是否可以注册
-		isOk: function() {
-			if (this.isuser && this.isemail && this.pwd.length > 8) {
-				this.isok = true;
-			} else {
-				this.isok = false;
-			}
+			this.isOk;
 		},
 
 		backOne: function() {
 			uni.navigateBack({
 				delta: 1
 			});
+		},
+
+		// //注册跳转测试
+		// signUp: function() {
+		// 	uni.navigateTo({
+		// 		url: '../login/login?user=' + this.user
+		// 	});
+		// },
+		//注册
+		signUp: function() {
+			if (this.isOk) {
+				uni.request({
+					url: this.serverUrl + '/signup/add',
+					data: {
+						name: this.user,
+						mail: this.email,
+						pwd: this.pwd
+					},
+					method: 'POST',
+					success: data => {
+						console.log(data);
+						let status = data.data.status;
+						if (status == 200) {
+							//注册成功跳转到登录界面
+							uni.navigateTo({
+								url: '../login/login?user=' + this.user
+							});
+						} else if (status == 500) {
+							uni.showToast({
+								title: '服务器出错啦！',
+								icon: 'none',
+								duration: 2000
+							});
+						}
+					}
+				});
+			}
 		}
 	}
 };
